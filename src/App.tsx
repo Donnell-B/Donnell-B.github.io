0007f0c66100e67741bba7f4df0d05957c6bea9b
Donnell-B <donnellburgess@outlook.com>
2024-12-31 20:28:28 +0000

    small commit, rebase to follow

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const connections = ["Github", "E-mail", ""];
 
+interface conn {
+  name: string;
+  link: string;
+  icon: string;
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>Hello? Is this thing on?</div>
+      <div className="card rounded-lg">
+        <div className="name">Donnell</div>
+        <div className="connections">
+          {connections.map((connections, index) => (
+            <span key={index}>{connections}</span>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
